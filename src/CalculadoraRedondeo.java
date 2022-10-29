d8c9a6102455196e738e188dadfe2d1c454b9a16
lcuetg00 <47599827+lcuetg00@users.noreply.github.com>
2022-10-29 03:37:17 +0200

    Actualizadas clases Consola y CalculadoraRedondeo
    -Terminada la funcionalidad de los métodos.
    -Dividido en funciones la funcionalidad de Consola.
    -Terminadoe l javadoc de las clases.

--- src/CalculadoraRedondeo.java
+++ src/CalculadoraRedondeo.java
@@ -1,9 +1,8 @@
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.math.RoundingMode;
 
 /**
- * Clase que contiene métodos estáticos para implementar las operaciones:
+ * Clase que contiene métodos estáticos para implementar las operaciones de la calculadora:
  * -Suma, resta, división, multiplicación con redondeo
  * -Módulo de la primera cantidad con la segunda
  * -Identificar cual de los dos números pasados por parámetro es mayor o menor
@@ -52,8 +51,8 @@ public class CalculadoraRedondeo {
 
     /**
      * Devuelve la multiplicación de dos BigDecimal redondeados al número de decimales especificado en el parámetro 'precision'
-     * @param num1 multiplicante 1
-     * @param num2 multiplicante 2
+     * @param num1 factor 1
+     * @param num2 factor 2
      * @param precision número de decimales a redondear
      * @return multiplicación de num1 y num2 redondeado al número de decimales especificado en el parámetro 'precision'
      */
@@ -62,6 +61,7 @@ public class CalculadoraRedondeo {
             throw new NullPointerException();
         }
         BigDecimal resultado = num1.multiply(num2);
+        resultado = resultado.setScale(precision, RoundingMode.HALF_UP);
         return resultado;
     }
     /**
@@ -75,12 +75,22 @@ public class CalculadoraRedondeo {
         if((num1==null) || (num2 == null)) { //Comprobación de parámetros de entrada
             throw new NullPointerException();
         }
-        BigDecimal resultado = num1.divide(num2,precision);
+        BigDecimal resultado = num1.divide(num2);
         resultado = resultado.setScale(precision, RoundingMode.HALF_UP);
         return resultado;
     }
 
-    public static boolean modulo(BigDecimal num1, BigDecimal num2) {
+    /**
+     * Comprueba si num1 es módulo de num2
+     * @param num1 número a realizar el módulo
+     * @param num2 módulo
+     * @return esModulo = true: si num1 módulo num2 == 0
+     *         esModulo = false: si num1 módulo num2 != 0
+     */
+    public static boolean moduloBigDecimal(BigDecimal num1, BigDecimal num2) {
+        if((num1==null) || (num2 == null)) { //Comprobación de parámetros de entrada
+            throw new NullPointerException();
+        }
         boolean esModulo = false;
         if(num1.remainder(num2).compareTo(BigDecimal.ZERO) == 0 ) {
             esModulo = true;
@@ -89,12 +99,50 @@ public class CalculadoraRedondeo {
         return esModulo;
     }
 
-    public static String recogerResultados(BigDecimal num1, BigDecimal num2, int precision) {
+    /**
+     * Crea un String describiendo si num1 es módulo de num2
+     * @param num1
+     * @param num2
+     * @return mensajeEsModulo: String que contiene en forma de texto si num1 es módulo de num2
+     */
+    public static String mensajeModuloBigDecimal(BigDecimal num1, BigDecimal num2) {
         if((num1==null) || (num2 == null)) { //Comprobación de parámetros de entrada
             throw new NullPointerException();
         }
-        StringBuilder textoFinal = new StringBuilder();
-        return textoFinal.toString();
+        String mensajeEsModulo = "";
+
+        if(CalculadoraRedondeo.moduloBigDecimal(num1,num2)) {
+            mensajeEsModulo = num1.toString() + " es módulo de " + num2.toString();
+        } else {
+            mensajeEsModulo = num1.toString() + " no es módulo de " + num2.toString();
+        }
+
+        return mensajeEsModulo;
+    }
+
+    /**
+     * Crea un String describiendo si num1 es mayor, menos o de igual valor que num2
+     * @param num1
+     * @param num2
+     * @return mensajeMayorMenorIgual: String que contiene en forma de texto si num1 es mayor, menor o de igual valor de num2
+     */
+    public static String mensajeEsMayor(BigDecimal num1, BigDecimal num2) {
+        if((num1==null) || (num2 == null)) { //Comprobación de parámetros de entrada
+            throw new NullPointerException();
+        }
+        int valorComparacion = -1;
+        String mensajeMayorMenorIgual = "";
+
+        valorComparacion = num1.compareTo(num2);
+        if(valorComparacion == 1) {
+            mensajeMayorMenorIgual = num1.toString() + " es mayor que " + num2.toString();
+        } else if (valorComparacion == -1) {
+            mensajeMayorMenorIgual = num2.toString() + " es mayor que " + num1.toString();
+        } else {
+            mensajeMayorMenorIgual = num1.toString() + " es igual que " + num2.toString();
+        }
+
+        return mensajeMayorMenorIgual;
     }
 
 }
--- src/Consola.java
+++ src/Consola.java
@@ -18,85 +18,115 @@ public class Consola {
     public static final int OPCIONSALIR = 8;
 
     //Formato de texto
-    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m"; //Para que el texto tenga un fondo verde
+    public static final String ANSI_RESET = "\u001B[0m"; //Para devolver el texto a la normalidad
 
     /**
-     * Funcionalidad de la consola //definir mejor
+     * Inicia la interfaz de la consola.
+     * Presenta el menú al usuario, lee sus entradas y realiza las operaciones necesarias
      */
     public void iniciarConsola() {
-        Scanner console = new Scanner(System.in);
-        int opcion = -1; //Variable que almacenará la opción que seleccionará el usuario
-        BigDecimal num1;
-        BigDecimal num2;
-        BigDecimal resultado;
-        Integer precision;
+        Scanner scanner = new Scanner(System.in);
+        int opcion = 0; //Almacenará la opción del menú que introducirá el usuario
+        int precision = 0; //Almacenará la precisión de los decimales que seleccione el usuario
+        BigDecimal num1; //Primer número introducido por teclado
+        BigDecimal num2; //Segundo número introducido por teclado
+        BigDecimal resultado; //Instancia que contendrá el resultado final de la operación seleccionada
 
         System.out.println("Ejercicio 1:");
 
+        imprimirMenu();
+
         while(opcion != OPCIONSALIR) {
             try {
-                System.out.println("1) Sumar");
-                System.out.println("2) Restar");
-                System.out.println("3) Multiplicar");
-                System.out.println("4) Dividir");
-                System.out.println("5) Modulo");
-                System.out.println("6) Comparar 2 números");
-                System.out.println("7) Realizar todas las operaciones");
-                System.out.println("8) Salir");
-                System.out.println("Escriba un número para seleccionar la opción que desees:");
-                opcion = console.nextInt();
-
-                this.clearConsola(); //Borramos el texto de las opciones
+                opcion = scanner.nextInt(); //Leemos la operación que quiere realizar el usuario
 
                 switch(opcion) {
                     case OPCIONSUMA:
                         System.out.println("Operación de suma");
                         System.out.println("Inserte el primer sumando");
-                        num1 = new BigDecimal(console.next());
+                        num1 = new BigDecimal(scanner.next());
                         System.out.println("Inserte el segundo sumando");
-                        num2 = new BigDecimal(console.next());
+                        num2 = new BigDecimal(scanner.next());
                         System.out.println("Inserte la precisión de los decimales");
-                        precision = console.nextInt();
+                        precision = scanner.nextInt();
 
                         resultado = CalculadoraRedondeo.sumaRedondeo(num1,num2,precision);
-
                         System.out.println("El resultado de la suma es " + resultado.toString());
 
 
                         break;
                     case OPCIONRESTA:
                         System.out.println("Operación de resta");
-                        System.out.println("Inserte el primer número");
-                        num1 = new BigDecimal(console.next());
+                        System.out.println("Inserte el minuendo de la resta");
+                        num1 = new BigDecimal(scanner.next());
                         System.out.println("Inserte el sustraendo de la resta");
-                        num2 = new BigDecimal(console.next());
+                        num2 = new BigDecimal(scanner.next());
                         System.out.println("Inserte la precisión de los decimales");
-                        precision = console.nextInt();
+                        precision = scanner.nextInt();
 
                         resultado = CalculadoraRedondeo.restaRedondeo(num1,num2,precision);
-
                         System.out.println("El resultado de la resta es " + resultado.toString());
 
                         break;
                     case OPCIONMULTIPLICACION:
+                        System.out.println("Operación de multiplicación");
+                        System.out.println("Inserte el primer número a multiplicar");
+                        num1 = new BigDecimal(scanner.next());
+                        System.out.println("Inserte el segundo número a multiplicar");
+                        num2 = new BigDecimal(scanner.next());
+                        System.out.println("Inserte la precisión de los decimales");
+                        precision = scanner.nextInt();
 
 
-                        //resultado = CalculadoraRedondeo.multiplicacionRedondeo(num1,num2,precision);
-                        //System.out.println("El resultado de la multiplciacion es " + resultado.toString());
+                        resultado = CalculadoraRedondeo.multiplicacionRedondeo(num1,num2,precision);
+                        System.out.println("El resultado de la multiplciacion es " + resultado.toString());
                         break;
                     case OPCIONDIVISION:
+                        System.out.println("Operación de division");
+                        System.out.println("Inserte el dividendo de la división");
+                        num1 = new BigDecimal(scanner.next());
+                        System.out.println("Inserte el divisor de la multiplicación");
+                        num2 = new BigDecimal(scanner.next());
+                        System.out.println("Inserte la precisión de los decimales");
+                        precision = scanner.nextInt();
 
-                        //resultado = CalculadoraRedondeo.divisionRedondeo(num1,num2,precision);
-                        //System.out.println("El resultado de la resta es " + resultado.toString());
+                        resultado = CalculadoraRedondeo.divisionRedondeo(num1,num2,precision);
+                        System.out.println("El resultado de la resta es " + resultado.toString());
                         break;
                     case OPCIONMODULO:
+                        System.out.println("Operación de módulo");
+                        System.out.println("Inserte el primer número");
+                        num1 = new BigDecimal(scanner.next());
+                        System.out.println("Inserte el número para el módulo de la operación");
+                        num2 = new BigDecimal(scanner.next());
 
+                        System.out.println(CalculadoraRedondeo.mensajeModuloBigDecimal(num1,num2));
                         break;
+
                     case OPCIONCOMPARACION:
+                        int valorComparacion = -1;
 
+                        System.out.println("Operación comparación de números");
+                        System.out.println("Inserte el primer número");
+                        num1 = new BigDecimal(scanner.next());
+                        System.out.println("Inserte el segundo número");
+                        num2 = new BigDecimal(scanner.next());
+
+                        valorComparacion = num1.compareTo(num2);
+                        System.out.println(CalculadoraRedondeo.mensajeEsMayor(num1,num2));
                         break;
+
+
                     case OPCIONTODAS:
+                        System.out.println("Realizar todas las operaciones");
+                        System.out.println("Inserte el primer número");
+                        num1 = new BigDecimal(scanner.next());
+                        System.out.println("Inserte el segundo número");
+                        num2 = new BigDecimal(scanner.next());
+                        System.out.println("Inserte la precisión de los decimales");
+                        precision = scanner.nextInt();
+                        System.out.println(this.recogerResultados(num1,num2,precision));
 
                         break;
                     case OPCIONSALIR:
@@ -104,54 +134,74 @@ public class Consola {
                         break;
                     default:
                         this.clearConsola();
+                        this.imprimirMenu();
                         System.out.println(ANSI_GREEN_BACKGROUND + "Opción tecleada incorrecta. Seleccione una opción con un número del 1 al 8" + ANSI_RESET);
-
                 }
 
+                this.imprimirMenu(); //Finalmente, volvemos a imprimir el menú
 
             } catch(InputMismatchException e) {
                 System.out.println(ANSI_GREEN_BACKGROUND + "Opción tecleada incorrecta. Seleccione una de las opciones disponibles" + ANSI_RESET);
-                console.nextLine();
+                scanner.nextLine();
                 this.clearConsola();
 
             } catch(NumberFormatException e) { //Formato de los numeros decimales
-                System.out.println(ANSI_GREEN_BACKGROUND + "Formato incorrecto del número. Volviendo al menú principal" + ANSI_RESET);
-                console.nextLine();
                 this.clearConsola();
+                System.out.println(ANSI_GREEN_BACKGROUND + "Formato incorrecto del número. Volviendo al menú principal" + ANSI_RESET);
+                this.imprimirMenu();
+                scanner.nextLine();
             }
-
-
-
-
-
         }
     }
 
-    /*
-    public void leerParametrosTeclado(BigDecimal num1, BigDecimal num2,Scanner consola) {
-
-
-    }
-    
+    /**
+     * Imprime el menú de opciones
      */
-
-
-/*
-    public void leerParametrosTeclado(Scanner consola) {
-
-        System.out.println("Inserte el primer número");
-        num1 = new BigDecimal(console.next());
-        System.out.println("Inserte el segundo número");
-        num2 = new BigDecimal(console.next());
-        System.out.println("Inserte la precisión de los decimales");
-        precision = console.nextInt();
-
+    public void imprimirMenu() {
+        System.out.println("_____________________________________________________________________");
+        System.out.println("1) Sumar");
+        System.out.println("2) Restar");
+        System.out.println("3) Multiplicar");
+        System.out.println("4) Dividir");
+        System.out.println("5) Modulo");
+        System.out.println("6) Comparar 2 números");
+        System.out.println("7) Realizar todas las operaciones");
+        System.out.println("8) Salir");
+        System.out.println("_____________________________________________________________________");
+        System.out.println("Formato de los números: decimales separados por un '.' (Ej: 123.456)");
+        System.out.println("Escriba un número para seleccionar la opción que desees:");
     }
-
- */
 
     /**
-     * Limpia todo el texto que aparece en la consola
+     * Realiza todas las operaciones disponibles en la calculadora y devuelve su resultado en forma de texto.
+     * @param num1
+     * @param num2
+     * @param precision
+     * @return texto con el resultado de realizar todas las operaciones de la calculadora realizadas sobre sus parámetros
+     */
+    public static String recogerResultados(BigDecimal num1, BigDecimal num2, int precision) {
+        if((num1==null) || (num2 == null)) { //Comprobación de parámetros de entrada
+            throw new NullPointerException();
+        }
+        StringBuilder textoFinal = new StringBuilder();
+        textoFinal.append("El resultado de la suma es ");
+        textoFinal.append(CalculadoraRedondeo.sumaRedondeo(num1,num2,precision).toString());
+        textoFinal.append("\nEl resultado de la resta es ");
+        textoFinal.append(CalculadoraRedondeo.restaRedondeo(num1,num2,precision).toString());
+        textoFinal.append("\nEl resultado de la multiplicación es ");
+        textoFinal.append(CalculadoraRedondeo.multiplicacionRedondeo(num1,num2,precision).toString());
+        textoFinal.append("\nEl resultado de la división es ");
+        textoFinal.append(CalculadoraRedondeo.divisionRedondeo(num1,num2,precision).toString());
+        textoFinal.append("\n");
+        textoFinal.append(CalculadoraRedondeo.mensajeModuloBigDecimal(num1,num2));
+        textoFinal.append("\n");
+        textoFinal.append(CalculadoraRedondeo.mensajeEsMayor(num1,num2));
+
+        return textoFinal.toString();
+    }
+
+    /**
+     * Limpia el texto que aparece en la consola
      */
     public void clearConsola() {
         System.out.print("\033[H\033[2J");
